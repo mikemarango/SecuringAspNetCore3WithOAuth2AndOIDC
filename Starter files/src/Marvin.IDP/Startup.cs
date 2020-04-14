bb789a6e029ad92ec5f40d5f2e8a2dab92748f1a
Michael Marango <mike.marango@gmail.com>
2020-04-14 13:37:30 +0300

    added Quick start UI

--- Starter files/src/Marvin.IDP/Startup.cs
+++ Starter files/src/Marvin.IDP/Startup.cs
@@ -9,48 +9,48 @@ using Microsoft.Extensions.Hosting;
 
 namespace Marvin.IDP
 {
-    public class Startup
+  public class Startup
+  {
+    public IWebHostEnvironment Environment { get; }
+
+    public Startup(IWebHostEnvironment environment)
     {
-        public IWebHostEnvironment Environment { get; }
-
-        public Startup(IWebHostEnvironment environment)
-        {
-            Environment = environment;
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // uncomment, if you want to add an MVC-based UI
-            //services.AddControllersWithViews();
-
-            var builder = services.AddIdentityServer()
-                .AddInMemoryIdentityResources(Config.Ids)
-                .AddInMemoryApiResources(Config.Apis)
-                .AddInMemoryClients(Config.Clients);
-
-            // not recommended for production - you need to store your key material somewhere secure
-            builder.AddDeveloperSigningCredential();
-        }
-
-        public void Configure(IApplicationBuilder app)
-        {
-            if (Environment.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            // uncomment if you want to add MVC
-            //app.UseStaticFiles();
-            //app.UseRouting();
-
-            app.UseIdentityServer();
-
-            // uncomment, if you want to add MVC
-            //app.UseAuthorization();
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapDefaultControllerRoute();
-            //});
-        }
+      Environment = environment;
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      // uncomment, if you want to add an MVC-based UI
+      services.AddControllersWithViews();
+
+      var builder = services.AddIdentityServer()
+          .AddInMemoryIdentityResources(Config.Ids)
+          .AddInMemoryApiResources(Config.Apis)
+          .AddInMemoryClients(Config.Clients);
+
+      // not recommended for production - you need to store your key material somewhere secure
+      builder.AddDeveloperSigningCredential();
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+      if (Environment.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      // uncomment if you want to add MVC
+      app.UseStaticFiles();
+      app.UseRouting();
+
+      app.UseIdentityServer();
+
+      // uncomment, if you want to add MVC
+      app.UseAuthorization();
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapDefaultControllerRoute();
+      });
+    }
+  }
 }
