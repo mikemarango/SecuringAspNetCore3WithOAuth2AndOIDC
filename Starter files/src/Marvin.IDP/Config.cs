da2591f7c2305629ce0f050af171c303ad3a4b1c
Michael Marango <mike.marango@gmail.com>
2020-04-14 16:05:06 +0300

    configured identity server

--- Starter files/src/Marvin.IDP/Config.cs
+++ Starter files/src/Marvin.IDP/Config.cs
@@ -2,27 +2,48 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
 namespace Marvin.IDP
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> Ids =>
-            new IdentityResource[]
-            { 
+  public static class Config
+  {
+    public static IEnumerable<IdentityResource> Ids =>
+        new IdentityResource[]
+        {
                 new IdentityResources.OpenId(),
                 new IdentityResources.Profile()
-            };
+        };
 
-        public static IEnumerable<ApiResource> Apis =>
-            new ApiResource[] 
-            { };
-        
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-        
-    }
+    public static IEnumerable<ApiResource> Apis =>
+        new ApiResource[]
+        { };
+
+    public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+          new Client
+          {
+            ClientName = "Image Gallery",
+            ClientId = "imagegalleryclient",
+            AllowedGrantTypes = GrantTypes.Code,
+            RedirectUris = new List<string>
+            {
+              "https://localhost:44366/signin-oidc"
+            },
+            AllowedScopes =
+            {
+              IdentityServerConstants.StandardScopes.OpenId,
+              IdentityServerConstants.StandardScopes.Profile
+            },
+            ClientSecrets =
+            {
+              new Secret("1b495357-db33-44d0-8b1d-b1293207a128".Sha256())
+            }
+          }
+        };
+
+  }
 }
